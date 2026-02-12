f09dd8f7f7b44327e507f615f77e45c653f4c833
Jcake456 <juancastelan546@gmail.com>
2026-02-12 10:39:58 -0600

    three commit

--- lib/scrrens/Login_screen.dart
+++ lib/scrrens/Login_screen.dart
@@ -1,22 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class loginScreen extends StatefulWidget {
-  const loginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<loginScreen> createState() => _loginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _loginScreenState extends State<loginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
+  //variable de control de contraseñas
+  final bool _obscuretext = true;
+
+  StateMachineController? _controller;
+  //SMI: STATE MACHINE INPUT
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMITrigger? _trigSuccess;
+  SMITrigger? _trigFail;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(children: [Container
-        (Expanded
-        (Child: RiveAnimation.asset('animated_login_bear')))],)
+    final size = MediaQuery.of(context).size;
 
-      )
+    return Scaffold(
+      backgroundColor: const Color(0xFFD6E2EA),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  'assets/oso.riv',
+                  fit: BoxFit.contain,
+                  stateMachines: ['Login Machine'],
+                  onInit: (artboard) {
+                    _controller = StateMachineController.fromArtboard(
+                      artboard,
+                      'Login Machine',
+                    );
+                    //verificar
+                    if (_controller == null) return;
+                    artboard.addController(_controller!);
+                    _isChecking = _controller!.findSMI('isChecking');
+                    _isHandsUp = _controller!.findSMI('isHandsUp');
+                    _trigSuccess = _controller!.findSMI('trigSuccess');
+                    _trigFail = _controller!.findSMI('trigFail');
+                  },
+                ),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: 'email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                if (_isHandsUp != null ) {
+                  _isHandsUp!.change(false)
+                if (_isChacking == null) return;
+                _isChecking!.change(true);
+                }
+              
+                obscureText: _obscuretext,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  prefixIcon: const Icon(Icons.lock),
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscuretext ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscuretext = !_obscuretext;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
